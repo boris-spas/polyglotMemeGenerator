27ea2537b34b4fd91b9295507375f79cf2cfd826
KonstantinLomotin <ke.lomotin@gmail.com>
2017-10-22 19:28:16 -0400

    Added multer library
    Signed-off-by: KonstantinLomotin <ke.lomotin@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+
 const app = express();
 
 const port = 3300;
+
+app.use(bodyParser.json());
 
 app.listen(port, function (argument) {
 	console.log("Hey! Port " + port);
