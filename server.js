fc5c24d23e77e87db0236873e0e301be09a00d93
KonstantinLomotin <ke.lomotin@gmail.com>
2017-10-23 09:15:04 -0400

    Implemented uploading and saving images
    Signed-off-by: KonstantinLomotin <ke.lomotin@gmail.com>

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ const port = 3300;
 
 app.use(bodyParser.json());
 
-app.listen(port, function (argument) {
-	console.log("Hey! Port " + port);
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
 });
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count 
+
+app.get("/", function (req, res) { 
+    res.sendFile(__dirname + "/index.html"); 
+}); 
+  
+app.post("/api/Upload", function (req, res) { 
+    upload(req, res, function (err) { 
+        if (err) { 
+            return res.end("Something went wrong!"); 
+        } 
+        return res.end("File successfully uploaded!"); 
+    }); 
+}); 
+
+app.listen(3300, function (a) { 
+    console.log("Listening to port 3300"); 
+}); 
