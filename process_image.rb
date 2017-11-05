779d9644e5da4ced26fad351beecde294fd38b91
KonstantinLomotin <ke.lomotin@gmail.com>
2017-11-05 10:38:46 -0500

    Added ruby code for generating memes with mini_magic gem. Added font Impact.ttx.
    Signed-off-by: KonstantinLomotin <ke.lomotin@gmail.com>

--- process_image.rb
+++ process_image.rb
@@ -1,5 +1,19 @@
-def test
-	print "hello from ruby script"
+require "mini_magick"
+
+def generateMeme(filename, top_caption, bot_caption)
+	img = MiniMagick::Image.open("../diagram.png")
+		img.combine_options do |c|
+		c.gravity "south"
+		c.draw "text 0,0 'Wololo'"
+		c.pointsize 40
+		c.stroke("black")
+		c.font "./fonts/Impact.ttf"
+		c.weight "bold"
+		c.fill("white")
+	end
+	meme_file = File.join(File.dirname(filename), File.basename("MEMEGEN" + filename))
+	img.write(meme_file)
+	meme_file
 end
 
 Truffle::Interop.export_method(:test)
