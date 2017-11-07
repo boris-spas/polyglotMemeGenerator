a2266ef49cae17a59411d23402b263c1c4b453a9
KonstantinLomotin <ke.lomotin@gmail.com>
2017-11-07 04:46:37 -0500

    Modified ruby memegen function
    Signed-off-by: KonstantinLomotin <ke.lomotin@gmail.com>

--- process_image.rb
+++ process_image.rb
@@ -4,16 +4,21 @@ def generateMeme(filename, top_caption, bot_caption)
 	img = MiniMagick::Image.open(filename)
 		img.combine_options do |c|
 		c.gravity "south"
-		c.draw "text 0,0 'Wololo'"
-		c.pointsize 40
+		c.draw "text 0,0 '" + bot_caption + "'"
+		c.gravity "north"
+		c.draw "text 0,0 '" + top_caption + "'"
+ 		c.pointsize 40
 		c.stroke("black")
 		c.font "./fonts/Impact.ttf"
 		c.weight "bold"
 		c.fill("white")
 	end
-	meme_file = File.join(File.dirname(filename), File.basename("MEMEGEN" + filename))
+	meme_file = File.join(File.dirname(filename), "MEMEGEN" + File.basename(filename))
 	img.write(meme_file)
 	meme_file
 end
 
 Truffle::Interop.export_method(:test)
+
+
+# generateMeme("../diagram.png", "wow, so diagram", "very graphic")
