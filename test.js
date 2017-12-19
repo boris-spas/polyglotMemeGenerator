1f3a8bbf12a603f55da272c7c8e364a71ec661cb
KonstantinLomotin <ke.lomotin@gmail.com>
2017-12-19 12:26:25 -0500

    Modified test script with code that triggers Truffle exceptions
    Signed-off-by: KonstantinLomotin <ke.lomotin@gmail.com>

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-var dbmClass = Java.type('DBManager');
-var dbm = dbmClass.getInstance();
-dbm.greetings(__dirname);
-console.log("Done!");
+// Simple test script
+// The code below triggers different Truffle exceptions
+
+var dbm = Java.type('DBManager').getInstance();
+
+// Testing byte[] (Exception 1)
+for (var i of dbm.testByteArray("hohoho")) {
+	console.log(i);
+}
+
+// Testing java.util.HashMap<Integer, String> (Exception 2)
+var imagesHashMap = dbm.getNImages(3);
